505ee44b3d942d65b7c39a831611f4b93bb6ee21
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-27 12:33:32 +0800

    Add stat retrieval

--- mainProjects/pokemon/pokemon-downloader.js
+++ mainProjects/pokemon/pokemon-downloader.js
@@ -3,12 +3,16 @@ import path from "path";
 import prompt from "./prompts.js";
 import fetch from "./fetch.js";
 
-
-
 const getPromptObject = async () => {
     const promptObject = await prompt();
     const pokemonObject = await fetch(promptObject.name);
-    console.log(pokemonObject);
+
+    const stats = {};
+    for (const stat of pokemonObject.stats) {
+        const [statName, statValue] = [stat.stat.name, stat.base_stat];
+        stats[statName] = statValue;
+    }
+    console.log(stats);
 };
 
 getPromptObject()
