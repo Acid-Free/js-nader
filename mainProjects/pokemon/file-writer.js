c7e629209388c579b89eb5d154115abfec366243
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-27 16:07:37 +0800

    Parallelize writeSprites

--- mainProjects/pokemon/file-writer.js
+++ mainProjects/pokemon/file-writer.js
@@ -1,4 +1,5 @@
 import fsp from "fs/promises";
+import { url } from "inspector";
 import { fetchImage } from "./fetch.js";
 
 const writeStats = async (pokemonObject, downloadDir) => {
@@ -18,21 +19,34 @@ const writeStats = async (pokemonObject, downloadDir) => {
 const writeSprites = async (pokemonObject, downloadDir) => {
     console.log("Downloading sprites...");
 
+    const spriteNames = [];
     const spriteURLs = [];
+    const spriteBuffers = {};
     const validImages = ["back_default", "back_female", "back_shiny", "back_shiny_female", "front_default", "front_female", "front_shiny", "front_shiny_female"];
 
     for (const sprite of Object.entries(pokemonObject.sprites))
-        if (validImages.includes(sprite[0])) {
+        if (validImages.includes(sprite[0]) && sprite[1] !== null) {
             try {
-                const result = await fetchImage(sprite[1]);
-                const buffer = Buffer.from(await result.arrayBuffer());
-                const fileDir = `${downloadDir}/${sprite[0]}.png`;
-                fsp.writeFile(fileDir, buffer);
-                console.log("✓", fileDir);
+                const currentFetch = fetchImage(sprite[1]);
+                spriteNames.push(sprite[0]);
+                spriteURLs.push(currentFetch);
             } catch (e) {
                 // sprites can be null
             }
         }
+
+    const urlResult = await Promise.all(spriteURLs);
+
+    for (let i = 0; i < urlResult.length; ++i) {
+        const buffer = Buffer.from(await urlResult[i].arrayBuffer());
+        spriteBuffers[spriteNames[i]] = buffer;
+    }
+
+    for (const [name, buffer] of Object.entries(spriteBuffers)) {
+        const fileDir = `${downloadDir}/${name}.png`;
+        fsp.writeFile(fileDir, buffer);
+        console.log("✓", fileDir);
+    }
 };
 
 const writeArtwork = async (pokemonObject, downloadDir) => {
