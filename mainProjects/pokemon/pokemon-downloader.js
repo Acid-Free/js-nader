1e02adbbca6d012a36b015bd8430de94ef32d36c
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-27 14:22:05 +0800

    Use promptObject properties for decision

--- mainProjects/pokemon/pokemon-downloader.js
+++ mainProjects/pokemon/pokemon-downloader.js
@@ -4,29 +4,38 @@ separate file download options
 connect prompt results to separated download functions
 don't allow file download if pokemon doesn't exist
 clean user input (pokemon name) (e.g. tolowercase, trim, etc)
+- create looping functionality using promptObject continue
+if invalid pokemon name, show that it doesn't exist
 */
 
 import fsp from "fs/promises";
 import fs from "fs";
-import path from "path";
 import prompt from "./prompts.js";
 import fetchPokemon from "./fetch.js";
 import { writeStats, writeSprites, writeArtwork } from "./file-writer.js";
 
 const getPromptObject = async () => {
-    const promptObject = await prompt();
-    const pokemonObject = await fetchPokemon(promptObject.name);
+    while (true) {
+        const promptObject = await prompt();
+        const pokemonObject = await fetchPokemon(promptObject.name);
+        const downloadDir = `./pokemon-storage/${pokemonObject.name}`;
 
-    const downloadDir = `./pokemon-storage/${pokemonObject.name}`;
-    if (!fs.existsSync(`${downloadDir}/`))
-        fsp.mkdir(downloadDir);
+        // creates a new directory if it doesn't exist yet
+        if (!fs.existsSync(`${downloadDir}/`))
+            fsp.mkdir(downloadDir);
 
-    writeStats(pokemonObject, downloadDir);
+        if (promptObject.actions.includes("Stats"))
+            writeStats(pokemonObject, downloadDir);
+        if (promptObject.actions.includes("Sprites"))
+            writeSprites(pokemonObject, downloadDir);
+        if (promptObject.actions.includes("Artwork"))
+            writeArtwork(pokemonObject, downloadDir);
 
-    writeSprites(pokemonObject, downloadDir);
-
-    // artwork
-    writeArtwork(pokemonObject, downloadDir);
+        if (!promptObject.continue) {
+            console.log("break!");
+            break;
+        }
+    }
 };
 
 getPromptObject()
