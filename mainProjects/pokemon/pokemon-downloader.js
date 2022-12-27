2e428ce3abf6f2c45859be07b1a86e2931f23671
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-27 13:39:54 +0800

    Add stats file save functionality

--- mainProjects/pokemon/pokemon-downloader.js
+++ mainProjects/pokemon/pokemon-downloader.js
@@ -8,19 +8,20 @@ const getPromptObject = async () => {
     const promptObject = await prompt();
     const pokemonObject = await fetchPokemon(promptObject.name);
 
-    const stats = {};
-    for (const stat of pokemonObject.stats) {
-        const [statName, statValue] = [stat.stat.name, stat.base_stat];
-        stats[statName] = statValue;
-    }
-
-    console.log(stats);
-
-
-    // sprites
     const downloadDir = `./pokemon-storage/${pokemonObject.name}`;
     if (!fs.existsSync(`${downloadDir}/`))
         fsp.mkdir(downloadDir);
+
+    let stats = "";
+    for (const stat of pokemonObject.stats) {
+        const [statName, statValue] = [stat.stat.name, stat.base_stat];
+        stats += `${statName}: ${statValue}\n`;
+    }
+
+    fsp.writeFile(`${downloadDir}/stats.txt`, stats);
+
+
+    // sprites
 
     const spriteURLs = [];
     const validImages = ["back_default", "back_female", "back_shiny", "back_shiny_female", "front_default", "front_female", "front_shiny", "front_shiny_female"];
