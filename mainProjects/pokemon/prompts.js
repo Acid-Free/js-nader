0cc706836acdd1a68a276bdd86b89a63cceb9f8c
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-27 14:44:53 +0800

    Refactor main function to three actions

--- mainProjects/pokemon/prompts.js
+++ mainProjects/pokemon/prompts.js
@@ -1,27 +1,34 @@
 import inquirer from "inquirer";
 
-const promptAll = async () => {
+const promptName = async () => {
     const response = await inquirer.prompt([
         {
             name: "name",
             message: "Pokemon name:",
             type: "input"
-        },
+        }
+    ]);
+};
+
+const promptActions = async () => {
+    const response = await inquirer.prompt([
         {
             name: "actions",
             message: "Select actions to perform:",
             choices: ["Stats", "Sprites", "Artwork"],
             type: "checkbox"
-        },
+        }
+    ]);
+};
+
+const promptContinue = async () => {
+    const response = await inquirer.prompt([
         {
             name: "continue",
             message: "Continue?",
             type: "confirm"
         }
     ]);
-
-    return response;
 };
 
-// const prompt = await promptAll();
-export default promptAll;
+export { promptName, promptActions, promptContinue };
