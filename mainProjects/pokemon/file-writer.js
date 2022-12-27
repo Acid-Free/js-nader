b513628bced0f7b9285eb08164f0cbe0e26cb171
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-27 14:09:24 +0800

    Refactor: create a separate file-writer.js

---
+++ mainProjects/pokemon/file-writer.js
@@ -0,0 +1,36 @@
+import fsp from "fs/promises";
+import { fetchImage } from "./fetch.js";
+
+const writeStats = async (pokemonObject, downloadDir) => {
+    let stats = "";
+    for (const stat of pokemonObject.stats) {
+        const [statName, statValue] = [stat.stat.name, stat.base_stat];
+        stats += `${statName}: ${statValue}\n`;
+    }
+
+    fsp.writeFile(`${downloadDir}/stats.txt`, stats);
+};
+
+const writeSprites = async (pokemonObject, downloadDir) => {
+    const spriteURLs = [];
+    const validImages = ["back_default", "back_female", "back_shiny", "back_shiny_female", "front_default", "front_female", "front_shiny", "front_shiny_female"];
+
+    for (const sprite of Object.entries(pokemonObject.sprites))
+        if (validImages.includes(sprite[0])) {
+            try {
+                const result = await fetchImage(sprite[1]);
+                const buffer = Buffer.from(await result.arrayBuffer());
+                fsp.writeFile(`${downloadDir}/${sprite[0]}.png`, buffer);
+            } catch (e) {
+                // sprites can be null
+            }
+        }
+};
+
+const writeArtwork = async (pokemonObject, downloadDir) => {
+    const result = await fetchImage(pokemonObject["sprites"]["other"]["official-artwork"]["front_default"]);
+    const buffer = Buffer.from(await result.arrayBuffer());
+    fsp.writeFile(`${downloadDir}/original-artwork.png`, buffer);
+};
+
+export { writeStats, writeSprites, writeArtwork };
--- mainProjects/pokemon/pokemon-downloader.js
+++ mainProjects/pokemon/pokemon-downloader.js
@@ -1,8 +1,17 @@
+/*
+TODO 
+separate file download options
+connect prompt results to separated download functions
+don't allow file download if pokemon doesn't exist
+clean user input (pokemon name) (e.g. tolowercase, trim, etc)
+*/
+
 import fsp from "fs/promises";
 import fs from "fs";
 import path from "path";
 import prompt from "./prompts.js";
-import fetchPokemon, { fetchImage } from "./fetch.js";
+import fetchPokemon from "./fetch.js";
+import { writeStats, writeSprites, writeArtwork } from "./file-writer.js";
 
 const getPromptObject = async () => {
     const promptObject = await prompt();
@@ -12,31 +21,12 @@ const getPromptObject = async () => {
     if (!fs.existsSync(`${downloadDir}/`))
         fsp.mkdir(downloadDir);
 
-    let stats = "";
-    for (const stat of pokemonObject.stats) {
-        const [statName, statValue] = [stat.stat.name, stat.base_stat];
-        stats += `${statName}: ${statValue}\n`;
-    }
+    writeStats(pokemonObject, downloadDir);
 
-    fsp.writeFile(`${downloadDir}/stats.txt`, stats);
-
-
-    // sprites
-
-    const spriteURLs = [];
-    const validImages = ["back_default", "back_female", "back_shiny", "back_shiny_female", "front_default", "front_female", "front_shiny", "front_shiny_female"];
-
-    for (const sprite of Object.entries(pokemonObject.sprites))
-        if (validImages.includes(sprite[0])) {
-            const result = await fetchImage(sprite[1]);
-            const buffer = Buffer.from(await result.arrayBuffer());
-            fsp.writeFile(`${downloadDir}/${sprite[0]}.png`, buffer);
-        }
+    writeSprites(pokemonObject, downloadDir);
 
     // artwork
-    const result = await fetchImage(pokemonObject["sprites"]["other"]["official-artwork"]["front_default"]);
-    const buffer = Buffer.from(await result.arrayBuffer());
-    fsp.writeFile(`${downloadDir}/original-artwork.png`, buffer);
+    writeArtwork(pokemonObject, downloadDir);
 };
 
 getPromptObject()
