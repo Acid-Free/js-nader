64110588e97470f3141ec4e8046552d6f66a1c09
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-12-29 12:32:19 +0800

    Original bonus solution

--- closure/bonus.js
+++ closure/bonus.js
@@ -4,42 +4,31 @@ const pokemonDownloader = () => {
   const download = async (pokemonName) => {
     pokemonName = pokemonName.trim().toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+    const pokemonRequest = await fetch(url);
 
-    // console.log(pokemonObject);
-    console.log(Object.entries(pokemonCache).length);
+    try {
+      const pokemonObject = await pokemonRequest.json();
 
-    if (pokemonName in pokemonCache)
-      console.log(`${pokemonName} entry found in cache.`);
-    else {
-      try {
-        const pokemonRequest = await fetch(url);
-        const pokemonObject = await pokemonRequest.json();
+      console.log(pokemonObject);
 
-        console.log(`${pokemonName} fetched from API.`);
-
+      if (pokemonName in pokemonCache)
+        console.log(`${pokemonName} entry found in cache.`);
+      else {
         pokemonCache[pokemonName] = pokemonObject;
-        console.log(Object.keys(pokemonCache));
-      } catch (e) {
-        console.log(`${pokemonName} is not a pokemon.`);
+        console.log(`${pokemonName} fetched from API.`);
       }
-    };
+    } catch (e) {
+      console.log(`${pokemonName} is not a pokemon.`);
+    }
+
+
   };
 
   return download;
 };
 
-const requester1 = async () => {
-  const requester = pokemonDownloader();
-  await requester("definitely a pokemon");
-  await requester("pikachu");
-  await requester("pikachu");
-  await requester("machop");
-  await requester("pikachu");
-  await requester("raichu");
-  await requester("pikachu");
-  await requester("pikachu");
-  await requester("pikachu");
-  await requester("pikachu");
-};
-
-requester1();
+const requester1 = pokemonDownloader();
+requester1("PikaChu");
+requester1("Pikachu");
+requester1("");
+requester1("");
