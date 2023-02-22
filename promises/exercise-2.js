1ef583bb2d215034abf8efe47a2365196ce6a332
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-02-22 11:06:20 +0800

    Update promises

--- promises/exercise-2.js
+++ promises/exercise-2.js
@@ -1,19 +1,25 @@
 console.log("Program started");
 
 const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Step 1 complete");
-    }, 3000);
+  setTimeout(() => {
+    resolve("Step 1 complete");
+  }, 1000);
 });
 
-myPromise.then((resolve) => {
-    console.log(resolve);
+// myPromise.then((resolve) => {
+//     console.log(resolve);
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Step 2 complete");
-        }, 3000);
-    });
-}).then((resolve) => {
-    console.log(resolve);
-});
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Step 2 complete");
+//         }, 3000);
+//     });
+// }).then((resolve) => {
+//     console.log(resolve);
+// });
+
+myPromise.then(done);
+
+function done() {
+  console.log("done");
+}
